94730864ff5c1093b525e17e227a795b2ad65a02
juliaortheden <orthedenjulia@gmail.com>
2017-04-28 13:59:04 +0200

    Working driveroverview

--- public/js/driverview.js
+++ public/js/driverview.js
@@ -1,11 +1,17 @@
 
     $(document).ready(function(){
         $(".add-row").click(function(){
-            var name = $("#name").val();
-            var email = $("#email").val();
-            var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
+            var way = $("#way").val();
+            var time = $("#time").val();
+            var recess = $("#recess").val();
+            var cost = $("#cost").val();
+            var ob = $("#ob").val();
+            var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + way + "</td><td>"
+                + time +"</td><td>" + recess + "</td><td>" + cost + "</td><td>" + ob +"</td></tr>";
             $("table tbody").append(markup);
         });
+
+
 
         // Find and remove selected table rows
         $(".delete-row").click(function(){
@@ -15,4 +21,42 @@
                 }
             });
         });
+
+        $(".change-row").click(function () {
+            var currentTD = $(this).parents('tr').find('td');
+            if ($(this).html() == 'Edit') {
+                $.each(currentTD, function () {
+                    $(this).prop('contenteditable', true)
+                });
+            } else {
+                $.each(currentTD, function () {
+                    $(this).prop('contenteditable', false)
+                });
+            }
+
+            $(this).html($(this).html() == 'Edit' ? 'Save' : 'Edit')
+
+
     });
+    });
+
+//do something similar to check input for drivers?!?! -->
+/*
+    //register
+    router.post('/register', function (req, res) {
+        var name = req.body.name;
+        var email = req.body.email;
+        var username = req.body.username;
+        var password = req.body.password;
+        var password2 = req.body.password2;
+
+        //validation
+        req.checkBody('name', 'Name is required').notEmpty();
+        req.checkBody('email', 'Email is required').notEmpty();
+        req.checkBody('email', 'Email is not valid').isEmail();
+        req.checkBody('username', 'Username is required').notEmpty();
+        req.checkBody('password', 'Password is required').notEmpty();
+        req.checkBody('password2', 'Passwords does not match').equals(req.body.password);
+
+        var errors = req.validationErrors();
+    }*/
